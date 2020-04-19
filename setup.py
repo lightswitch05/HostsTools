9634b0b8f549e0ad1e611fc919b832ee12676a7d
Daniel <daniel@developerdan.com>
2020-04-19 15:02:48 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="HostTools",
+    version="0.0.1",
+    url="https://github.com/lightswitch05/HostsTools",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
