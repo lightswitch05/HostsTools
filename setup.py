674b416118a744496f6eabc650ac91625fbe7531
Daniel <daniel@developerdan.com>
2020-04-22 21:20:10 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import setuptools
 
 setuptools.setup(
-    name="HostTools",
-    version="0.0.1",
+    name="HostsTools",
+    version="0.0.2",
     url="https://github.com/lightswitch05/HostsTools",
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
